6bbe4743e8509b899823c85cf3d01c3d230d95d5
Julius Polar <julius.polar@protonmail.com>
2025-09-09 16:05:00 +0200

    added eslint-react

--- eslint.config.react.mjs
+++ eslint.config.react.mjs
@@ -1,12 +1,36 @@
+import eslintReact from "@eslint-react/eslint-plugin";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 import basicConfig from "./eslint.config.mjs";
 
 export default [
   ...basicConfig,
+  eslintReact.configs["recommended-typescript"],
   reactHooks.configs["recommended-latest"],
   reactRefresh.configs.vite,
   jsxA11y.flatConfigs.strict,
+
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+
+  {
+    overrides: [
+      {
+        files: ["*.{jsx,tsx}"],
+        rules: {
+          "unicorn/no-null": "off",
+        },
+      },
+    ],
+  },
 ];
